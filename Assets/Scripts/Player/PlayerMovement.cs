b22c4e0d9be9ae8c7d72011d1bd0bc7adedfa70c
jptguindaca <jptguindaca@gmail.com>
2025-10-19 14:39:20 +0100

    Initial commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Config")]
+    [SerializeField] private float speed;
+
+
+    private Movimento actions;
+    private Vector2 moveDirection;
+    private Rigidbody2D rb;
+    private Animator anim;
+    void Awake()
+    {
+        actions = new Movimento();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        ReadMovement();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    
+    private void OnEnable()
+    {
+        actions.Enable();
+        
+    }
+
+    private void OnDisable()
+    {
+        actions.Disable();
+    }
+
+    private void ReadMovement()
+    {
+        moveDirection = actions.Movement.Move.ReadValue<Vector2>().normalized;
+
+        if(moveDirection == Vector2.zero)
+        {
+            return;
+        }
+
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + moveDirection * (speed * Time.fixedDeltaTime));
+    }
+
+}
